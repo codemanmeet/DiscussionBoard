0300da8520d13743a2bcfac7b2ce0ddf3dbbd2f8
Manmeet Saluja <manmeet.saluja@u.nus.edu>
2015-12-08 15:33:23 +0530

    authentication is required to create new posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :post_finder, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@posts = Post.all.order("created_at DESC")
